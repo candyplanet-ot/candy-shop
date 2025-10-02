53c8318c4b04760d1f42c9eb5cd66cc069c003a3
candyplanet-ot <candyplanet.sete@gmail.com>
2025-10-02 17:30:08 +0530

    Add files via upload

--- src/components/SEOHead.tsx
+++ src/components/SEOHead.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useLocation } from "react-router-dom";
 import { Helmet } from 'react-helmet-async';
 
@@ -13,19 +12,3 @@ const SEOHead = () => {
 };
 
 export default SEOHead;
-=======
-import { useLocation } from "react-router-dom";
-import { Helmet } from 'react-helmet-async';
-
-const SEOHead = () => {
-  const location = useLocation();
-  const isAdminPath = location.pathname.startsWith('/admin') || location.pathname === '/login' || location.pathname === '/signup';
-  return (
-    <Helmet>
-      <meta name="robots" content={isAdminPath ? "noindex, nofollow" : "index, follow"} />
-    </Helmet>
-  );
-};
-
-export default SEOHead;
->>>>>>> cf3b96f63dd429b0a17bbe128b6c7a693ae364f5
